cb776b11b20e6362964cd7915caed3da2d9f960d
vincenzooo <kovorama@gmail.com>
2019-04-08 04:04:38 -0400

    started reordering of readers

--- dataIO/dicts.py
+++ dataIO/dicts.py
@@ -19,7 +19,79 @@ def make_file_dict(filelist,delimiter='_',index=0):
 
     return filedic
 
-def pop_kw(kws,defaults,keylist=None):
+
+filterByKey2 = lambda data,keys : {key: data[key] for key in keys if key in data}
+	
+def pop_kw(kws,keylist=None,defaults=None):
+    """A multiple pop function for a dictionary, hopefully useful to filter kws and manage function **kwargs parameters.
+
+    Return a dictionary of values corresponding to kws[k] for k in keylist by popping
+      the element from kwargs.
+    If k is present in a dictionary `defaults`, take the values from `defaults` if not present in kws.
+      return a dictionary {key:val} with val from `kws` or `defaults` if in any of them.  
+    Another way of seeing the return value is as a copy of `defaults` where values are replaced 
+      (after popping them) from same key in kws. 
+
+
+        kwargs={'dog':'bau','cat':'miao'}
+        defs={'dog':'barf','sheep':'bee'}
+        
+        #pop 'cat' and 'ship' if they are in kwargs or take them from defaults if there,
+        #  put them in res
+        >>> res=pop_kw(kwargs,defaults=defs) 
+        >>> kwargs
+        Out[103]: {'cat': 'miao'}
+        >>> res
+        Out[104]: {'dog': 'barf', 'sheep': 'bee'}
+        
+        #pop 'cat' and 'ship' if they are in kwargs or take them from defaults if there,
+        #  put them in res
+        >>> res=pop_kw(kwargs,['cat','sheep'],defs)  
+        >>> kwargs
+        Out[103]: {}
+        >>> res
+        Out[104]: {'cat': 'miao', 'dog': 'bau', 'sheep': 'bee'}
+
+
+    A typical usage can be dividing arbitrary **kwargs between different subroutines,
+      where the subroutines might not accept **kwargs, and be constrained to specific list of parameters.
+      To pass kwargs in this circumstance do inside the routine:
+    
+    Module `inspect` can be used to retrieve subfunction parameter names and make a safe call: 
+        
+       import inspect
+       pars=list(inspect.signature(f1).parameters) #give list of all accepted parameters in f1
+       res1=f1(arg1,**pop_kw(kwargs,pars)) #note that pars will have keys 'args' and 'kwargs' if
+                            # f1 accepts arbitrary arguments. kws and defaults with same name can be
+                            #  in conflict
+    
+    In general, if **kws are passed to a function that calls two other subs, each one with optional arguments, it can be used as (see full example in `dataIO.dicts.example_pop_kw`):
+
+        def example_pop_kw(**kwargs):
+
+            # here if passed directly with `res0=f1(arg1,**kwargs)` gives error because mes2 is not a defined parameter
+            res1=f1(arg1,**pop_kw(kwargs,['mes1'],{'mes1':'blah!'})) #only mes1 is passed to res1 with default if not in kwargs:
+            res2=f2(arg1,**kwargs)  #other remaining kwargs can be safely passed to f2 
+            
+    Completely reviewed 2019/04/08, changed interface and function of keylist.
+            
+    
+    """
+    res={}
+    keys=list(defaults.keys())
+    for k in keys:
+        res[k]=kws.pop(k,defaults[k])
+    
+    if keylist is not None:
+        if isinstance(keylist,str): keylist=[keylist] #make list if only one element
+        for k in keylist:
+            if k in kws:
+                res[k]=kws.pop(k)
+            
+    return res
+	
+    '''
+	def pop_kw(kws,defaults,keylist=None):
     """A multiple pop function for a dictionary, hopefully useful to filter kws and manage function parameters.
     
     a copy of `defaults` is returned where values are stripped from kws if a key is present. 
@@ -47,14 +119,18 @@ def pop_kw(kws,defaults,keylist=None):
             warnings.warn(str(e),RuntimeWarning)
             
     return res
+	'''
 
 def strip_kw(kws,funclist,split=False,**kwargs):
-    """Given a list of functions and a dictionary of parameters,
-       
+    """was in theory a safer version of the first pop_kw.
+           I am not sure in what it was supposed to be "safer",
+       but it was accepting functions as input.
+	   It might have been used a negligible amount of times.
+	   
        sub_kw=strip_kw(kws,[sub],def1=val1,def2=val2)"""
     
     res=[]
-    if callable(funclist):
+    if callable(funclist): #if scalar, makes it a one element list
         funclist=[funclist]
     for func in funclist:
         f=inspect.getfullargspec(func)
@@ -70,46 +146,74 @@ def strip_kw(kws,funclist,split=False,**kwargs):
         
     return res
     
+def test_filterByKey():
+	eegKeys = ["FP3", "FP4"]
+	gyroKeys = ["X", "Y"]
+
+	# 'Foo' is ignored
+	data = {"FP3": 1, "FP4": 2, "X": 3, "Y": 4, "Foo": 5}
+	print(filterByKey(data,eegKeys))
+	
 def example_pop_kw():
+    
+    def f1(arg,mes1='--'):
+        print (arg,mes1)
+    def f2(arg,mes2='--'):
+        print (arg,mes2)
+    
     def function(arg1,**kwargs):
-        '''a function with 1 arg'''
-        def sub1(arg,mes1='--'):
-            print (arg,mes1)
-        def sub2(arg,mes2='--'):
-            print (arg,mes2)
-            
-        res1=sub1(arg1,**pop_kw(kwargs,{'mes1':'blah!'}))
+        '''a function with 1 arg. pop_kw is used to manipulate and smartly filter `**kwargs` between two functions,
+          `f1(arg,mes1='--')` and `f2(arg,mes2='--')`, so not to pass unexpected arguments.
+          `res0=f1(arg1,**kwargs)` #would give error.'''
         
-        res2=sub2(arg1,**kwargs)
+        print ("arguments passed to functions: \n arg:",arg1,"\nkwargs:",kwargs)
+        
+        # here if passed directly with `res0=f1(arg1,**kwargs)` because mes2 is not a defined parameter
+        res1=f1(arg1,**pop_kw(kwargs,'mes1',{'mes1':'blah!'})) #only mes1 is passed to res1 with default if not in kwargs:
+        
+        #other remaining kwargs can be passed to f2
+        res2=f2(arg1,**kwargs)     
+        
         return res1,res2
     
-    print (function('This is:',mes1='mes1!'))
-    print (function('This is:',mes1='mes1!',mes2='mese2!'))
+
+    function('This is:',mes2='mes2!')
+    print('**')
+    function('This is:',mes1='mes1!')
+    print('**')
+    function('This is:',mes1='mes1!',mes2='mese2!')
+    print('**')
+    function('This is:',mes1='mes1!',mes2='mese2!')
     
+#from dataIO.dicts import pop_kw, strip_kw
 def test_pop_kw():
+    
     #without keylist return a dictionary
-    kwargs={'cane':'dog','units':'mm'}
-    kk={'units':3,'nsigma':1}
-    print('start:\n',kwargs,kk,'\n---\n')
-    res=pop_kw(kwargs,kk)
-    print(kwargs,'\n',kk,'\n',res)
+    kwargs={'dog':'bau','cat':'miao'}
+    defs={'dog':'barf','sheep':'bee'}
+    res=pop_kw(kwargs,defaults=defs)  #remove from kwargs the keys in defs assigning default if they don't exist in kwargs
+    print(res)
+    # {'sheep': 'bee', 'dog': 'bau'}
     print("#---------------\n")
     
-    #keylist sorts values, a list is returned
-    kwargs={'cane':'dog','units':'mm'}
-    kk={'units':3,'nsigma':1}
-    print('start:\n',kwargs,kk,'---\n')
-    res=pop_kw(kwargs,kk,['nsigma','units'])
-    print(kwargs,'\n',kk,'\n',res)
+    #keylist sorts values, a list is returned (is it useful? cannot you filter the returned dict?)
+    kwargs={'dog':'bau','cat':'miao'}
+    defs={'dog':'barf','sheep':'bee'}
+    res=pop_kw(kwargs,['cat','sheep'],defs)
+    print(res)
+    # {'sheep': 'bee', 'cat': 'miao', 'dog': 'bau'}
     print("#---------------\n")    
     
+    
     #same, but one of the keys is invalid, ignore
-    kwargs={'cane':'dog','units':'mm'}
-    kk={'units':3,'nsigma':1}
-    print('start:\n',kwargs,kk,'---\n')
-    res=pop_kw(kwargs,kk,['nsigma','units','gatto'])
-    print(kwargs,'\n',kk,'\n',res)
-    print("#---------------\n")    
+    kwargs={'dog':'bau','cat':'miao'}
+    defs={'dog':'barf','sheep':'bee'}
+    res=pop_kw(kwargs,['nsigma','units','gatto'],defs) #gatto doesn't exist in kwargs
+    print(res)
+    # ignoring key invalid in dict {'units': 'mm', 'nsigma': 1}:
+    #'
+    #{'sheep': 'bee', 'dog': 'bau'}
+    print("#------------")
     
 if __name__=="__main__":
     test_pop_kw()
---
+++ pySurf/readers_dev/readers/format_reader.py
@@ -0,0 +1,81 @@
+"""  This module contains functions able to read raw data from different formats.
+     Returns data and header as a couple, with minimal data processing, functions are about formats, 
+     not the specific instrument or specific header, that are handled in calling functions
+     (e.g. from instrument_reader). 
+	 
+
+2018/09/26 New refactoring of data reader. Vincenzo Cotroneo vcotroneo@cfa.harvard.edu"""
+
+import numpy as np
+from dataIO.fn_add_subfix import fn_add_subfix
+
+def read_fits(fitsfile):
+    """ Generic fits reader, returns data,x,y.
+    
+    header is ignored. If `header` is set to True is returned as dictionary."""
+    from astropy.io import fits
+    
+    a=fits.open(fitsfile)
+    meta=a[0]   
+    data=meta.data
+    a.close()
+ 
+    x= meta.x if hasattr(meta,'x') else np.arange(data.shape[1])
+    y= meta.y if hasattr(meta,'y') else np.arange(data.shape[0])
+       
+    raw = meta.header if hasattr(meta,'header') else meta
+    return (data,x,y),raw
+	
+def read_sur(file):
+    from read_sur_files import readsur
+    """read a sur file using read_sur_files, that is expected to return a structure
+     res.points, .xAxis, .yAxis"""
+
+    res = readsur(file) #in readsur the default is False.
+    data,x,y = res.points,res.xAxis,res.yAxis
+    del res.points
+    del res.xAxis
+    del res.yAxis
+
+    return (data,x,y),res #stripped of all data information
+	
+from pySurf.points import get_points,points_find_grid,resample_grid
+
+def read_csv_points(wfile,*args,**kwargs):
+    """Read a processed points file in format x,y,z as csv output of analysis routines."""
+    w0=get_points(wfile,*args,**kwargs)
+    w=w0.copy()
+    raw=w0
+    x,y=points_find_grid(w,'grid')[1]
+    pdata=resample_grid(w,matrix=True)
+    return (pdata,x,y),raw
+	
+def csv_zygo_reader(wfile,*args,**kwargs):
+    """Read a processed points file in format x,y,z as csv output of analysis routines."""
+    pass
+	
+
+
+# location of test input and output data,
+#  for economy during development, hard coded path.
+testfolder=r'G:\My Drive\libraries\python\userKov3\pySurf\test' 
+	
+def test_reader(file,reader,outfolder=None,**kwargs):
+    """called without `raw` flag, return data,x,y"""
+    import os
+    import matplotlib.pyplot as plt
+    from  pySurf.data2D import plot_data
+    
+    df=os.path.join(testfolder,file)
+    res,header=reader(df,**kwargs)
+    print("returned values",[r.shape for r in res],header)
+    
+    plot_data(res[0],res[1],res[2])
+    if outfolder is not None:
+        if outfolder == "" : 
+            display(plt.gcf()) 
+        else: 
+            outname=os.path.join(testfolder,outfolder,os.path.basename(df))
+            os.makedirs(os.path.dirname(outname),exist_ok=True)
+            plt.savefig(fn_add_subfix(outname,'','.png'))
+    return res,header
