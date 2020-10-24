99a489b4b00af1f265b782df18db0ae9eb040525
Vincenzo Cotroneo <vincenzo.cotroneo.mi@gmail.com>
2020-10-25 00:11:25 +0200

    changes to markers and polygon plot functions

--- plotting/plot_positions.py
+++ plotting/plot_positions.py
@@ -1,24 +1,57 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def plot_poly(mh,ls='',ms=''):
-    """plot a polygon defined by an array of vertices as list of x and y.
-    linestyle and markerstyle can be passed"""
+"""Plot markers and shapes in dependance on positions of a group of points."""
+
+'''
+def plot_poly(v,ls='',ms=''):
+    """plot a polygon defined by a list of vertices `v` as coordinate couples.
+    linestyle and markerstyle can be passed.
+    """
     
     mh=np.array(mh)
-    mh=np.vstack([mh,mh[0,:]])
+    # duplicate first point at the end: 
+    mh=np.vstack([mh,mh[0,:]]) 
+    # plt.plot return a list of one element
+    # if there are markers, plot them:
     if ms is not None:
-        plt.plot(mh[:-1,0],mh[:-1,1],ms)
+        m = plt.plot(mh[:-1,0],mh[:-1,1],ms)[0]
     
-    return plt.plot(mh[:,0],mh[:,1],ls)
+    #plot lines
+    return plt.plot(mh[:,0],mh[:,1],ls)[0]
+'''
+    
+def plot_poly(v,*args,**kwargs):
+    """plot a polygon defined by a list of vertices `v` as coordinate couples.
+    Just a thin wrapper around `plt.plot`, it just accepts points in transpose form and adds a copy of first point at the."""
+    
+    v=np.array(v)
+    # duplicate first point at the end: 
+    v=np.vstack([v,v[0,:]]) 
+    # plt.plot return a list of one element
+    
+    #plot lines
+    return plt.plot(*v.T,*args,**kwargs)
 
-def plot_rect(xs,ys,ls='',ms=''):
-    """plot a polygon defined by an array of vertices as list of x and y.
-    linestyle and markerstyle can be passed"""
+def plot_rect(c1,c2,*args,**kwargs):
+    """plot a polygon defined by a 2 element list with coordinates of opposite corners.
+    linestyle and markerstyle can be passed.
+    es. 
     
-    mh=[[xs[0],ys[0]],[xs[1],ys[0]],[xs[1],ys[1]],[xs[0],ys[1]]]
-    return plot_poly(mh,ls=ls,ms=ms)
+    2020/10/13 changed to coordinates of opposite corners from [xspan,yspan]"""
     
+    mh=[[c1[0], c1[1]],[c2[0],c1[1]],[c2[0],c2[1]],[c1[0],c2[1]]]
+    #mh=[[xs[0],ys[0]],[xs[1],ys[0]],[xs[1],ys[1]],[xs[0],ys[1]]]
+    return plot_poly(mh,*args,**kwargs)
+    
+def plot_circle_ROI(c,r,color='r'):
+    """plot center and circunference (color)."""
+    plt.plot(c[0],c[1],'o',color=color,alpha=0.5)
+    c1=plt.Circle(c,r,fill=None,color=color)
+    plt.gca().add_artist(c1)
+    #plt.draw()
+    return c1
+
 def circle3points(fiducials):
     """Calculate circle passing by three points.
     Return center and radius."""
@@ -31,18 +64,10 @@ def circle3points(fiducials):
     w /= y-x
     c = (x-y)*(w-abs(w)**2)/2j/w.imag-x
     return (-c.real, -c.imag), abs(c+x)
-
-def plot_circle_ROI(c,r,color='r'):
-    """plot center and circunference (color)."""
-    plt.plot(c[0],c[1],'o',color=color,alpha=0.5)
-    c1=plt.Circle(c,r,fill=None,color=color)
-    plt.gca().add_artist(c1)
-    #plt.draw()
-    return c1
-
+    
 def plot_circle3points(fiducials):
     """given three fiducial points, plot them and the circle passing by them.
-    Return line (for points) and circle artists."""
+    Return line (passing through points) and circle artists."""
     
     fid=np.array(fiducials)
     pfid=plt.plot(fid[:,0],fid[:,1],'x')
@@ -53,16 +78,26 @@ def plot_circle3points(fiducials):
     cir=plt.Circle(c,r,fill=0)
     ax.add_artist(cir)
     return pfid,cir
-
+    
 def plot_positions(fiducials,positions,labels=None,*args,**kwargs):
-    """plot circle on three point and position of measurements.
-	This can be replaced by a combination of markers and plot_circle3points."""
+    """
+    Obsolete: use a combinatio of plt commands, or make better separated functions to plot circles, markers, labels, etc.
+    
+    plot circle on three point `fiducials` and mark the three points, plot `labels` at `positions`.
+    fiducials is a list of three coordinate couples.
+    positions a list of n points in same format.
+    label can be set to empty string to generate numeric labels (useless, can pass e.g. `[str(i).zfill(2) for i in range(N)]`).
+    
+    This can be replaced by a combination of markers and plot_circle3points.
+    Praticamente una funzione bruttina che plotta un combinazione inutile di roba.
+    Si potrebbe notevolmente migliorare.
+    """
     
     pfid,cir=plot_circle3points(fiducials)
-
     pos=np.array(positions)    
     fs=kwargs.pop('fontsize',20)
-    if labels is not None:
+    
+    if labels is not None:    
         if labels=='':
             labels=['%3i'%ll for ll in range(pos.shape[0])]
         plab=[]
@@ -74,18 +109,24 @@ def plot_positions(fiducials,positions,labels=None,*args,**kwargs):
     return pfid,cir,ppos,plab
 
 if __name__=="__main__":
-	fid=((77.312, 87.499), (77.899,102.718), (29.208,103.132))
-	positions=[[58.158,97.466],
-	[59.652,104.989],
-	[63.947,95.724],
-	[68.501,97.656],
-	[69.928,88.653],
-	[48.077,88.663],
-	[48.029,109.160]]
+    fiducials=((77.312, 87.499), (77.899,102.718), (29.208,103.132))
+    positions=[[58.158,97.466],
+    [59.652,104.989],
+    [63.947,95.724],
+    [68.501,97.656],
+    [69.928,88.653],
+    [48.077,88.663],
+    [48.029,109.160]]
 
-	plt.clf()
-	pfid,cir,ppos,plab=plot_positions(fiducials,positions,c='y',ls='none',marker='o',fontsize=20,labels='')
+    plt.clf()
+    pfid,cir,ppos,plab=plot_positions(fiducials,positions,c='y',ls='none',marker='o',fontsize=20,labels='')
 
-	#if circle is not wanted can be removed
-	cir.set_color('r')
+    #if circle is not wanted can be removed
+    cir.set_color('r')
+    r = plot_rect([40,90],[50,100])
+    r[0].set_linestyle(":")
+    
+    p = plot_poly([[45,95],[55,100],[35,105]])
+    p[0].set_linestyle("-.")
+    plt.draw()
 
