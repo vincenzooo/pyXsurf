3ea92b4b9f78ba8facf45702637dc46422c1f878
Vincenzo Cotroneo <vincenzo.cotroneo.mi@gmail.com>
2021-02-20 08:10:42 +0100

    separate functionality in routine to read pars from strings rather than file

--- dataIO/read_pars_from_namelist.py
+++ dataIO/read_pars_from_namelist.py
@@ -1,3 +1,13 @@
+def namelist_from_string(l,separator="="):
+    """extract namelist parameters from a list of strings (e.g. file lines). """
+    l=[ll.strip() for ll in l if ll.count(separator) == 1 ]  #consider only valid line (one and only one = sign in the line).
+    dict={}
+    for ll in l:
+        s=ll.split(separator)
+        dict[s[0].strip()]=s[1].strip()
+    return dict    
+
+
 def read_pars_from_namelist(filename,separator="="):
     '''Read a set of parameters from a (fortran-like) namelist in file FILENAME.
     Return a dictionary. All values are read as strings.
@@ -7,13 +17,8 @@ def read_pars_from_namelist(filename,separator="="):
         l=filename.readlines()
     else:
         l=open(filename,'r').readlines()
-    l=[ll.strip() for ll in l if ll.count(separator) == 1 ]  #consider only valid line (one and only one = sign in the line).
-    dict={}
-    for ll in l:
-        s=ll.split(separator)
-        dict[s[0]]=s[1]
-    return dict
 
+    return namelist_from_string(l,separator=separator)
 
 if __name__=="__main__":
     file=r'E:\work\WTDf\geoSettings.txt'
