310f4a2829f538ec233dc5b7996ca67bd0f05c74
Vincenzo Cotroneo <vincenzo.cotroneo.mi@gmail.com>
2021-07-14 13:00:02 +0200

    Improvement of Dlist as subclass of (Working) superlist

--- dataIO/superlist.py
+++ dataIO/superlist.py
@@ -20,6 +20,32 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pdb
 
+class Superlist(list):
+    """A list of pySurf.Data2D objects on which unknown operations are performed serially."""    
+    
+    def __getattr__(self,name,*args,**kwargs): 
+        #print("GETATTR!")
+        attr = [object.__getattribute__(name) for object in self]
+        print("ATTR:",attr,*args,**kwargs)
+        print("SELF,name",self,name)
+        
+        if hasattr(attr[0], '__call__'):
+            print("call methods of items")
+            # devo costruire una nuova funzione che preso un oggetto
+            # lista ritorna un oggetto lista ottenuto dal valore restituito dalla funzione su ogni elemento.
+            def newfunc(*args, **kwargs):
+                attr = [object.__getattribute__(name) for object in self]
+                result = [a(*args, **kwargs) for a in attr]
+                return result
+            return newfunc
+        else:
+            # return list of attributes
+            print("return atribute of items")
+            return attr
+            
+
+## DEVELOPMENT VERSIONS
+
 class superlist1(list):
     """Test class that vectorizes methods. Basic, implements properties,
         but not methods."""
@@ -106,7 +132,7 @@ class superlist3(list):
 
         return result  #it works as a property. As method, this returns a list of methods, but since the method is called as sl.method(),
         #gives an error as it tries to call the list.
-        # deve ritornare una funzione che ritorna una lista.        
+        # deve ritornare una funzione che ritorna una lista.  
 
 class superlist4(list):
     """Test class that vectorizes methods.  """
@@ -127,6 +153,130 @@ class superlist4(list):
                 #stessa, perche' se ritorna lista non posso applicare ulteriori
                 #metodi.
 
+class superlist_alt(list):
+    """2021/07/14 da https://stackoverflow.com/questions/2704434/intercept-method-calls-in-python anche testata in Dlist."""
+    
+    def __getattribute__(self,name):
+        attr = object.__getattribute__(self, name)
+        if hasattr(attr, '__call__'):
+            def newfunc(*args, **kwargs):
+                print('before calling %s' %attr.__name__)
+                result = attr(*args, **kwargs)
+                print('done calling %s' %attr.__name__)
+                return result
+            return newfunc
+        else:
+            return attr
+            
+class superlist5(list):
+    """2021/07/14 da Dlist, versione funzionante con proprieta' e metodi, portata qui e non testata."""
+    
+    def __getattr__(self,name,*args,**kwargs):  #originariamente usava __getattribute__, che riceve attributo
+        # prima di chiamarlo (quindi anche se gia' esistente).
+        #attr = object.__getattr__(self, name) #questo non funziona
+        
+        attr = [object.__getattribute__(name) for object in self]
+        print("ATTR:",attr,*args,**kwargs)
+        print("SELF,name",self,name)
+        
+        if hasattr(attr[0], '__call__'):
+            print("call methods of items")
+            # devo costruire una nuova funzione che preso un oggetto
+            # lista ritorna un oggetto lista ottenuto dal valore restituito dalla funzione su ogni elemento.
+            def newfunc(*args, **kwargs):
+                attr = [object.__getattribute__(name) for object in self]
+                result = [a(*args, **kwargs) for a in attr]
+                return result
+            return newfunc
+            
+            """
+            # per quale motivo restituire una funzione?
+
+                if hasattr(attr[0], '__call__'):
+                    return [a(*args,**kwargs) for a in attr]  # e' una generica lista 
+                else:
+                    # return list of attributes
+                    return Dlist(attr) #attr
+
+            Il codice quasi funziona, nel senso che genera plot, ma da anche errore. In realta' chiamando D.plot  (senza parentesi), la lista viene restituita comunque.
+            Questo perche' __getattr__ viene chiamato da qualcosa che si aspetta di vedere restituita una funzione, non un valore.
+
+            Per questo l'equivalente unidimensionale era:
+            
+            def newfunc(*args, **kwargs):
+                #pdb.set_trace()
+                #print('before calling %s' %attr.__name__)
+                result = attr(*args, **kwargs)
+                #print('done calling %s' %attr.__name__)
+                return result
+            return newfunc
+            """
+        else:
+            # return list of attributes
+            print("return atribute of items")
+            return Dlist(attr) #attr
+            
+class Superlist6(list):
+    """Working version for debug of getattr/getattribute.
+    print messages."""    
+    
+    def __getattr__(self,name,*args,**kwargs): 
+        print("GETATTR!")
+        attr = [object.__getattribute__(name) for object in self]
+        print("ATTR:",attr,*args,**kwargs)
+        print("SELF,name",self,name)
+        
+        if hasattr(attr[0], '__call__'):
+            print("call methods of items")
+            # devo costruire una nuova funzione che preso un oggetto
+            # lista ritorna un oggetto lista ottenuto dal valore restituito dalla funzione su ogni elemento.
+            def newfunc(*args, **kwargs):
+                attr = [object.__getattribute__(name) for object in self]
+                result = [a(*args, **kwargs) for a in attr]
+                return result
+            return newfunc
+        else:
+            # return list of attributes
+            print("return atribute of items")
+            return attr
+    
+    def __getattribute__(self,name):
+        print("GETATTRIBUTE!")
+        attr = object.__getattribute__(self, name)
+        if hasattr(attr, '__call__'):
+            def newfunc(*args, **kwargs):
+                print('before calling %s' %attr.__name__)
+                result = attr(*args, **kwargs)
+                print('done calling %s' %attr.__name__)
+                return result
+            return newfunc
+        else:
+            return attr
+    """"""
+
+
+
+def test_superlist_dlist(D):
+    #create a dlist D
+    # test
+    print('dlist:')
+    print(D)
+    print('\ntest property (D.name):')
+    print(D.name)
+    print('\ntest method (D.plot):')
+    print(D.plot())
+
+    # existing method
+    D.plot
+
+    # existing method call
+    D.plot()
+      
+    # non existing 
+    D.cane
+
+
+         
 def test_superlist(cls,obj=None):
 
     print ('test class ',cls)
