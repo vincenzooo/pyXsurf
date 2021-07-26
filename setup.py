f804893237f4db42044749c9577971c6a2c0674b
Vincenzo Cotroneo <vincenzo.cotroneo.mi@gmail.com>
2021-07-26 09:01:14 +0200

    empty structure

--- setup.py
+++ setup.py
@@ -1,6 +1,50 @@
 from setuptools import setup
+
 setup(
-  name='docs',
+  name='pyXsurf',
   version='0.1.0',
-  packages=['docs'],
+  packages=['pyxsurf'],
 )
+
+'''
+#from https://docs.python.org/3/distutils/setupscript.html
+
+from distutils.core import setup
+
+setup(    name='pyXsurf',
+    version='0.1.1',
+    packages=['pyXsurf'],
+    description="Python library for X-Ray Optics, Metrology Data Analysis and Telescopes Design.",
+    url='https://github.com/vincenzooo/pyXSurf',
+    author='Vincenzo Cotroneo',
+    author_email='vincenzo.cotroneo@inaf.it',
+    #url='https://github.com/vincenzooo/pyXSurf',
+    )
+'''     
+     
+'''
+
+from setuptools import setup
+setup(
+    name='pyXsurf',
+    version='0.1.1',
+    packages=['pyXsurf'],
+    description="Python library for X-Ray Optics, Metrology Data Analysis and Telescopes Design.",
+    url='https://github.com/vincenzooo/pyXSurf',
+    author='Vincenzo Cotroneo',
+    author_email='vincenzo.cotroneo@inaf.it',
+    keywords=['surfaces', 'metrology', 'PSD'],
+    tests_require=[
+        'pytest'
+    ],
+    package_data={
+        # include json and pkl files
+        '': ['*.json', 'models/*.pkl', 'models/*.json'],
+    },
+    include_package_data=False,
+    python_requires='>=3'
+)
+
+# from 
+
+'''
