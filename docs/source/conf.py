ea55222339e7501dbd5fc0ba5b13c01872ae58d2
Vincenzo Cotroneo <vincenzo.cotroneo.mi@gmail.com>
2023-08-18 10:10:45 +0200

    changed docs requirements.txt to make readthedocs work

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -38,7 +38,7 @@ release = 'v1.5.6'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['myst-parser',
+extensions = ['myst_parser',
               #"myst_nb",
               'sphinx.ext.duration',
               'sphinx_gallery.load_style',
