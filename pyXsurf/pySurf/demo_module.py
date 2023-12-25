f304d84d3ad13f6f69a51795e0fc62d3ea719c70
robeyes <152690113+robeyes@users.noreply.github.com>
2023-12-25 16:52:39 -0600

    Add binary file comparison example

--- pyXsurf/pySurf/demo_module.py
+++ pyXsurf/pySurf/demo_module.py
@@ -15,3 +15,9 @@ def common_data_function():
     data_file = files("pyXsurf.data").joinpath("common_data.txt")
     with as_file(data_file) as data_file:
         return data_file.read_text()
+
+
+def image_generate_file(destination_file):
+    # This is a dummy function that resembes writing an image file to disk.
+    with open(destination_file, "wb") as fh:
+        fh.write(b"Hello World!")
--- test/units/pySurf/test_demo_module.py
+++ test/units/pySurf/test_demo_module.py
@@ -1,3 +1,6 @@
+import filecmp
+from pathlib import Path
+
 import pyXsurf.pySurf.demo_module as psdm
 
 
@@ -16,3 +19,19 @@ def test_common_data_function():
 def test_test_data(test_path):
     with open(test_path / "data" / "test_data.txt", "r") as f:
         assert "test data\n" == f.read()
+
+
+def test_image_generate_file(test_path, tmp_path):
+    # Prepare output directory
+    output_tmp_dir: Path = tmp_path / "units.pySurf.test_demo_module"
+    output_tmp_dir.mkdir(parents=True, exist_ok=True)
+    # Run function which generates file
+    psdm.image_generate_file(output_tmp_dir / "test_image.png")
+
+    # Compare generated file with golden image
+    input_test_dir: Path = test_path / "units" / "pySurf" / "data"
+    assert filecmp.cmp(
+        input_test_dir / "test_golden_image.png",
+        output_tmp_dir / "test_image.png",
+        shallow=False,
+    )
