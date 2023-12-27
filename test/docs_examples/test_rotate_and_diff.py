e9e5a1ca7032ef9bdb5aa997d17d43a30da2f329
Vincenzo Cotroneo <vincenzo.cotroneo.mi@gmail.com>
2023-12-27 22:19:22 +0100

    test folder hardcoded as relative path

--- test/docs_examples/test_rotate_and_diff.py
+++ test/docs_examples/test_rotate_and_diff.py
@@ -1,14 +1,23 @@
 import subprocess
+
+"""
+main runs a Python .py script. script path is hardcoded as relative path from from this file.
+
+> python test_rotate_and_diff.py
+"""
+
 import sys
 from pathlib import Path
 
-project_root = Path(__file__).parent.parent.parent
-script_dir = project_root / "docs" / "source" / "examples"
+project_root = Path(__file__).parent.parent.parent  # root relative to this file
+script_dir = (
+    project_root / "docs" / "source" / "examples"
+)  # location of script to run wrt root
 script_path = script_dir / "rotate_and_diff.py"
 
 
 def main():
-    """simply run the example script."""
+    """Run system Python interpreter on the test script corresponding to this test."""
     print("Hello from test_rotate_and_diff.py!")
     subprocess.run([sys.executable, script_path])
 
