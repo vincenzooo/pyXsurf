4ae89fe25abb01620102a5ce8fa5bacdd38df550
robeyes <152690113+robeyes@users.noreply.github.com>
2023-12-27 19:25:50 -0600

    Put all tests under pytest

--- test/docs_notebooks/conftest.py
+++ test/docs_notebooks/conftest.py
@@ -6,8 +6,12 @@ import pytest
 
 
 @pytest.fixture(scope="module")
-def notebooks_path():
-    project_root = Path(__file__).parent.parent.parent
+def notebooks_path(project_root):
     notebooks_dir = project_root / "docs" / "source" / "notebooks"
     with patch.dict(os.environ, {"PYXSURF_NOTEBOOKS_PATH": str(notebooks_dir)}):
         yield notebooks_dir
+
+
+@pytest.fixture(scope="module")
+def test_docs_notebooks_path(test_path):
+    return test_path / "docs_notebooks"
--- test/docs_notebooks/test_pySurf_nb_demo.py
+++ test/docs_notebooks/test_pySurf_nb_demo.py
@@ -1,31 +1,34 @@
+from pathlib import Path
+
 import pytest
 from testbook import testbook
+from testbook.client import TestbookNotebookClient
 
 
 @pytest.fixture(scope="module")
-def notebook(notebooks_path):
+def notebook(notebooks_path: Path):
     nb_dir = notebooks_path / "nb_demo"
     with testbook(
         nb_dir / "pySurf_nb_demo.ipynb",
         execute=True,
-        # Set the run_path to the notebook directory so that
-        # it can find the test data with in that directory.
+        # Set the current working directory to the notebook directory
+        # so that it can find the test data with in that directory.
         resources={"metadata": {"path": nb_dir}},
     ) as nb:
         yield nb
 
 
-def test_function(notebook):
+def test_function(notebook: TestbookNotebookClient):
     assert "pySurf function" == notebook.ref("function")()
 
 
-def test_data_function(notebook):
+def test_data_function(notebook: TestbookNotebookClient):
     assert "pySurf functionality data\n" == notebook.ref("data_function")()
 
 
-def test_common_data_function(notebook):
+def test_common_data_function(notebook: TestbookNotebookClient):
     assert "common functionality data\n" == notebook.ref("common_data_function")()
 
 
-def test_nb_test_data(notebook):
+def test_nb_test_data(notebook: TestbookNotebookClient):
     assert "nb test data\n" == notebook.ref("nb_test_data")()
