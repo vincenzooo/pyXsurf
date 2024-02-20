3aec845de4bf989644f73ce5ec88855ac2f1e4df
Vincenzooo <vincenzo.cotroneo@inaf.it>
2024-02-20 07:14:29 +0100

    updated labels and title mechanism in plots

--- source/pyProfile/profile_class.py
+++ source/pyProfile/profile_class.py
@@ -890,7 +890,7 @@ class PSD(Profile):
                 
     def plot(self,*args,**kwargs):
         u=kwargs.pop('units',self.units)
-        return plot_psd(self.x,self.y,units=u,*args,**kwargs)
+        return plot_psd(self.x,self.y,units=u,label=self.name,*args,**kwargs)
     
     def rms_power(self,plot=False,*args,**kwargs):
         """Calculate rms slice power as integral of psd. If plot is set also plot the whole thing."""
