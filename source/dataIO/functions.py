6e64da6a6c32d5bd93e3fbe54afcbc04bdb03430
Vincenzooo <vincenzo.cotroneo@inaf.it>
2024-03-05 17:44:07 +0100

    refactoring with of functions for redistribution of kwargs moved to dataIO.functions or dicts

--- source/dataIO/functions.py
+++ source/dataIO/functions.py
@@ -14,3 +14,70 @@ def update_docstring(func,source,delimiter=''):
     doc0="" if func.__doc__ is None else func.__doc__  # retrieve current docstring
     func.__doc__='\n'.join([doc0,delimiter,source.__name__+str(inspect.signature(source)),source.__doc__])
     return func
+  
+
+def strip_kw(kws,funclist,split=False,exclude=None,**kwargs):
+    """ Enhanced version of pop_kw, kw can be extracted from inspection of a function (or a list  of functions).
+    Defaults can be passed as kwargs.
+    `exclude` is a list of keys that are kept in kws unchanged
+       and not returned.
+    
+    2020/05/26 added `exclude`.
+    Use case is presented from problems with psd_analysis.
+    `psd2d.psd_analysis` calls `strip_kw(kwargs,psd2d_analysis,title="")`. 
+    `ps2d_analysis` has keyword `units`, that is not used in this case (used, only if called with `analysis=True`). Here we want to preserve `units` to strip it later.
+    
+    Old notes:
+        was in theory a safer version of the first pop_kw.
+           I am not sure in what it was supposed to be "safer",
+       but it was accepting functions as input.
+	   It might have been used a negligible amount of times.
+
+       sub_kw=strip_kw(kws,[sub],def1=val1,def2=val2)"""
+
+    res=[]
+    if callable(funclist): #if scalar, makes funclist a one element list
+        funclist=[funclist]    
+        
+    for func in funclist:
+        f=inspect.getfullargspec(func)
+        defaults=kwargs
+        l = len(f.defaults) if f.defaults is not None else 0
+        fdic=dict(zip(f.args[-l:],f.defaults))
+        res.append(pop_kw(kws,fdic,exclude=exclude))
+
+    if not split:  #merge dictionaries, don't ask me how
+        tmp = {}
+        [tmp.update(d) for d in res]
+        res=tmp
+
+    return res
+    
+def prep_kw(function, kwargs, strip = False):
+    """
+    return a dictionary only of `kwargs` pertaining to `function`.
+    Arguments can be stripped from kwargs as they are used, if strip is set to True, otherwise kwargs is left unchanged.
+    This can be useful to check against extraneous args, e.g.:    
+    intended use (inside a function called with **kwargs):
+    
+    function (*args, **kwargs):
+    
+        kwargs = kwargs.copy() # this is copied to avoid modification as side effect, can be avoided if you don't need a final check, in that case, simply remove strip = True from subfunction calls
+
+        a = subfunction1(**prep_kw(function1, kwargs, strip=True))
+        b= subfunction2(**prep_kw(function2, kwargs, strip=True))
+        
+        if len(kwargs):  # check there are not unused keywords.
+            raise TypeError("invalid arguments in function.")
+    
+    see also https://stackoverflow.com/questions/26534134/python-pass-different-kwargs-to-multiple-functions
+    
+    There might be a problem with multiple-level nested functions, for example if instead of plt.plot there is a function which itself calls other functions which accept **kwargs."""
+    
+    if not strip:
+        kwargs = kwargs.copy()
+    kwlist = list(inspect.signature(function).parameters)
+    return {k: kwargs.pop(k) for k in dict(kwargs) if k in kwlist}
+  
+  
+  
