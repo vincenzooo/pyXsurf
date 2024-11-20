e9725e14fd515bd607d122a8d2d26956b2ab7a99
Vincenzooo <vincenzo.cotroneo@inaf.it>
2024-11-20 15:38:41 +0100

    syntax upgrade to more correct handling of types

--- source/pySurf/readers/read_sur_files.py
+++ source/pySurf/readers/read_sur_files.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import os
 
 def fread(fid, nelements, dtype):
-    if dtype is str:
+    if dtype == str:
         dt = np.uint8  # WARNING: assuming 8-bit ASCII for np.str!
     else:
         dt = dtype
@@ -57,38 +57,38 @@ def readsur(filepath,raw=False):
     res=Res()
 
     res.signature           =   freadChar(fileID,12);               #1
-    res.format              =   fread(fileID,1,'int16');            #2
-    res.objNum              =   fread(fileID,1,'int16');            #3
-    res.version             =   fread(fileID,1,'int16');            #4
-    res.objType             =   fread(fileID,1,'int16');            #5
+    res.format              =   fread(fileID,1,np.int16);            #2
+    res.objNum              =   fread(fileID,1,np.int16);            #3
+    res.version             =   fread(fileID,1,np.int16);            #4
+    res.objType             =   fread(fileID,1,np.int16);            #5
     res.objName             =   freadChar(fileID,30);               #6
     res.operatorName        =   freadChar(fileID,30);               #7
 
-    res.materialCode        =   fread(fileID,1,'int16');            #8
-    res.acquisitionType     =   fread(fileID,1,'int16');            #9
-    res.rangeType           =   fread(fileID,1,'int16');            #10
-    res.specialPoints       =   fread(fileID,1,'int16');            #11
-    res.absoluteHeights     =   fread(fileID,1,'int16');            #12
-    res.gaugeResolution     =   fread(fileID,1,'single'); #fread(fileID,1,'float');            #13
+    res.materialCode        =   fread(fileID,1,np.int16);            #8
+    res.acquisitionType     =   fread(fileID,1,np.int16);            #9
+    res.rangeType           =   fread(fileID,1,np.int16);            #10
+    res.specialPoints       =   fread(fileID,1,np.int16);            #11
+    res.absoluteHeights     =   fread(fileID,1,np.int16);            #12
+    res.gaugeResolution     =   fread(fileID,1,np.single); #fread(fileID,1,'float');            #13
 
     freadChar(fileID,4);        #reserved space. for unknown reason this works in matlab, not in python  #14
 
     #data format of pints
-    res.sizeOfPoints        =   fread(fileID,1,'int16');            #15
+    res.sizeOfPoints        =   fread(fileID,1,np.int16);            #15
 
-    res.zMin                =   fread(fileID,1,'int32');            #16
-    res.zMax                =   fread(fileID,1,'int32');            #17
+    res.zMin                =   fread(fileID,1,np.int32);            #16
+    res.zMax                =   fread(fileID,1,np.int32);            #17
 
     #Number of points per axis
-    res.xPoints             =   int(fread(fileID,1,'int32'))            #18
-    res.yPoints             =   int(fread(fileID,1,'int32'))            #19
+    res.xPoints             =   int(fread(fileID,1,np.int32))            #18
+    res.yPoints             =   int(fread(fileID,1,np.int32))            #19
 
-    res.totalNumberOfPoints =   fread(fileID,1,'int32');            #20
+    res.totalNumberOfPoints =   fread(fileID,1,np.int32);            #20
     
     #Distance between points
-    res.xSpacing            =   fread(fileID,1,'single') #'float'); #21
-    res.ySpacing            =   fread(fileID,1,'single') #'float'); #22
-    res.zSpacing            =   fread(fileID,1,'single') #'float'); #23
+    res.xSpacing            =   fread(fileID,1,np.single) #'float'); #21
+    res.ySpacing            =   fread(fileID,1,np.single) #'float'); #22
+    res.zSpacing            =   fread(fileID,1,np.single) #'float'); #23
 
     #Name of axis
     res.xName               =   freadChar(fileID,16);               #24
@@ -106,41 +106,41 @@ def readsur(filepath,raw=False):
     res.zLengthUnit         =   freadChar(fileID,16);               #32
 
     #Skaling of distance between points
-    res.xUnitRatio          =   fread(fileID,1,'single') #'float'); #33
-    res.yUnitRatio          =   fread(fileID,1,'single') #'float'); #34
-    res.zUnitRatio          =   fread(fileID,1,'single') #'float'); #35
+    res.xUnitRatio          =   fread(fileID,1,np.single) #'float'); #33
+    res.yUnitRatio          =   fread(fileID,1,np.single) #'float'); #34
+    res.zUnitRatio          =   fread(fileID,1,np.single) #'float'); #35
 
-    res.imprint             =   fread(fileID,1,'int16') #'float'); #36
-    res.inverted            =   fread(fileID,1,'int16') #'float'); #37
-    res.levelled            =   fread(fileID,1,'int16') #'float'); #38
+    res.imprint             =   fread(fileID,1,np.int16) #'float'); #36
+    res.inverted            =   fread(fileID,1,np.int16) #'float'); #37
+    res.levelled            =   fread(fileID,1,np.int16) #'float'); #38
     freadChar(fileID,12);       #obsolete                           #39
 
     #timestamp
-    res.startSeconds        =   fread(fileID,1,'int16');            #40
-    res.startMinutes        =   fread(fileID,1,'int16');            #41
-    res.startHours          =   fread(fileID,1,'int16');            #42
-    res.startDays           =   fread(fileID,1,'int16');            #43
-    res.startMonths         =   fread(fileID,1,'int16');            #44
-    res.startYears          =   fread(fileID,1,'int16');            #45
-    res.startWeekDay        =   fread(fileID,1,'int16');            #46
-    res.measurementDuration =   fread(fileID,1,'single') #'float'); #47
+    res.startSeconds        =   fread(fileID,1,np.int16);            #40
+    res.startMinutes        =   fread(fileID,1,np.int16);            #41
+    res.startHours          =   fread(fileID,1,np.int16);            #42
+    res.startDays           =   fread(fileID,1,np.int16);            #43
+    res.startMonths         =   fread(fileID,1,np.int16);            #44
+    res.startYears          =   fread(fileID,1,np.int16);            #45
+    res.startWeekDay        =   fread(fileID,1,np.int16);            #46
+    res.measurementDuration =   fread(fileID,1,np.single) #'float'); #47
 
     freadChar(fileID,10);       #obsolete                           #48
 
     #Size of comment field
-    res.commentSize         =   fread(fileID,1,'int16');            #49
-    res.privateSize         =   fread(fileID,1,'int16');            #50
+    res.commentSize         =   fread(fileID,1,np.int16);            #49
+    res.privateSize         =   fread(fileID,1,np.int16);            #50
 
     res.clientZone          =   freadChar(fileID,128);              #51
 
     #Axis offset
-    res.xOffset             =   fread(fileID,1,'single') #'float'); #52
-    res.yOffset             =   fread(fileID,1,'single') #'float'); #53
-    res.zOffset             =   fread(fileID,1,'single') #'float'); #54
+    res.xOffset             =   fread(fileID,1,np.single) #'float'); #52
+    res.yOffset             =   fread(fileID,1,np.single) #'float'); #53
+    res.zOffset             =   fread(fileID,1,np.single) #'float'); #54
 
     #temperature scale
-    res.tSpacing            =   fread(fileID,1,'single') #'float'); #55
-    res.tOffset             =   fread(fileID,1,'single') #'float'); #56
+    res.tSpacing            =   fread(fileID,1,np.single) #'float'); #55
+    res.tOffset             =   fread(fileID,1,np.single) #'float'); #56
     res.tStepUnit           =   freadChar(fileID,13);               #57
     res.tAxisName           =   freadChar(fileID,13);               #58
 
@@ -149,9 +149,9 @@ def readsur(filepath,raw=False):
 
     #read datapoints
     if res.sizeOfPoints == 16:                                       #61
-        res.points          =   fread(fileID,-1,'int16')
+        res.points          =   fread(fileID,-1,np.int16)
     elif res.sizeOfPoints == 32:
-        res.points          =   fread(fileID,-1,'int32')        
+        res.points          =   fread(fileID,-1,np.int32)        
     else:
         raise ValueError("data lack property sizeOfPoints (or file is in the wrong format)")
 
