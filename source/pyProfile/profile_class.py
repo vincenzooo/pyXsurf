set consistently default units to None, solves bug with d.psd().avgpsd().plot()

source/pyProfile/profile_class.py
@@ -259,7 +259,7 @@ class Profile(object):  #np.ndarrays
 
 
     def __init__(self,x=None,y=None,file=None,reader=None,
-        scale = (1.,1.), units=["",""],name=None,*args,**kwargs):
+        scale = (1.,1.), units=None,name=None,*args,**kwargs):
         """can be initialized with data; x,y; file; file, x
         if x is provided, they override x from data if matching number of elements, 
         or used as range if two element (error is raised in case of ambiguity)."""
@@ -339,7 +339,8 @@ class Profile(object):  #np.ndarrays
         #goes indented.
 
         if np.size(units) == 1:
-            units=[units,units]
+            if units is not None:
+                units=[units,units]
         self.units=units
         if name is not None:
             self.name=name
@@ -853,10 +854,11 @@ class Profile(object):  #np.ndarrays
         print(s)
         return s
     
-    
+    from dataIO.outliers import remove_outliers as _remout
     def remove_outliers(self,fill_value=np.nan,correct=False,*args,**kwargs):
         """use dataIO.outliers.remove_outliers to remove outliers.
         If correct is True, replace outliers with interpolated values (fill_value is ignored)."""
+        
         res=self.copy()
         m = dataIO.outliers.remove_outliers(res.y,*args,**kwargs)
         # import pdb
@@ -868,7 +870,7 @@ class Profile(object):  #np.ndarrays
             res.y[~m] = fill_value
         return res
         
-    remove_outliers=update_docstring(remove_outliers,dataIO.outliers.remove_outliers)
+    remove_outliers=update_docstring(remove_outliers,_remout)
 
     '''
     def align_interactive(self,other,find_transform=find_affine):

source/pySurf/data2D_class.py
@@ -286,7 +286,10 @@ class Data2D(object):  # np.ndarrays
 
         self.data, self.x, self.y = data, x, y
 
-        self.units = units
+        if np.size(units) == 1:
+            if units is not None:
+                units=[units,units,units]
+        self.units = units  # can be None, string, or 3-element array
         if name is not None:
             self.name = name
         elif file is not None:
@@ -759,7 +762,10 @@ class Data2D(object):  # np.ndarrays
     def projection(self, axis = 0, *args, **kwargs):
         """avg, returns x and y. Can use data2D.projection keywords `span` and `expand` to return data ranges."""
         from pyProfile.profile_class import Profile
-        res = Profile(self.y, projection(self.data, axis=axis, *args,**kwargs), units = [self.units[1], self.units[2]], name = " ".join([self.name,"avg along axis %i"%(axis)])) 
+        u = self.units if self.units is not None else ["","",""]
+        res = Profile(self.y, projection(self.data, axis=axis, *args,**kwargs), 
+                      units = ([u[1], u[2]] if self.units is not None else None), 
+                      name = " ".join([self.name,"avg along axis %i"%(axis)])) 
     
         # print(self)
         return res